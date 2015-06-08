2c9fe30d1885e4f51c94c6315de5f12a2886bf22
Lukas Hackenberg <lukas.hackenberg@gmx.at>
2015-06-08 03:03:22 +0200

    Added shutdown button

--- server/views.py
+++ server/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 
 from server import app
 from server.models import Note
@@ -14,3 +14,12 @@ def index():
 def get_note(id):
     note = Note.query.get(id)
     return render_template("note.html", note=note)
+
+
+@app.route("/shutdown", methods=["GET", "POST"])
+def shutdown():
+    func = request.environ.get("werkzeug.server.shutdown")
+    if func is None:
+        raise RuntimeError("Not running with the Werkzeug Server")
+    func()
+    return "Server shutting down..."
