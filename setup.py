ed1dcee7c2ad43223c8d066d431a4f052165e169
Lukas Hackenberg <lukas.hackenberg@gmx.at>
2015-06-10 01:55:47 +0200

    Restructured setup.py and added a MANIFEST.in file.
    This was necessary to include static/ and templates/ directories into the
    distribution packages.

--- setup.py
+++ setup.py
@@ -28,13 +28,14 @@ setup(
     version="0.0.1",
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
-    install_requires=["flask", "flask_sqlalchemy", "markdown"],
     entry_points={
         "console_scripts": [
             "markbook = markbook:cmdline.main",
         ],
     },
-    tests_require=["pytest"],
     cmdclass={"test": PyTest},
+    install_requires=["flask", "flask_sqlalchemy", "markdown"],
+    tests_require=["pytest"],
     license="MIT",
+    zip_safe=False,
 )
